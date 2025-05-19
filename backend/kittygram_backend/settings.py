8211434473dcf408cff0fe20b437edb7099e5121
Miron Tolkanitsa <miron.tolkanitsa@gmail.com>
2025-05-20 02:40:11 +0300

    fix static

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -100,7 +100,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/' 
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
+STATIC_ROOT = '/static'
  
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = BASE_DIR / 'media'
